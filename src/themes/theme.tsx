ea7a225efaf7ae3a7db7821a9ced214613416b7e
Ruan Fernandes Guimarães <ruan_guimaraes2004@hotmail.com>
2024-09-08 13:43:08 -0300

    Theming start

---
+++ src/themes/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#FFB147',
+		},
+		secondary: {
+			main: '#41AFEF',
+		},
+	},
+});
+
+export default theme;
