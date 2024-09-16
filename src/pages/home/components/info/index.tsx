8b9af2610870da081cee9155b7a5a043449e830d
Ruan Fernandes Guimarães <ruan_guimaraes2004@hotmail.com>
2024-09-16 09:15:57 -0300

    spelling fix

--- src/pages/home/components/info/index.tsx
+++ src/pages/home/components/info/index.tsx
@@ -20,7 +20,7 @@ const Info: React.FC<InfoProps> = ({ mb }) => {
 		const infos = [
 			'Vici Online is an all-in-one platform that lets you play and create 2D MMOs. The platform also offers seamless publishing and sharing tools, allowing you to connect with a large community of players and creators. Join Vici Online today and unleash your creativity.',
 			"This unique game is developed by gamers for gamers and encourages community feedback and suggestions. Players have the sources to create their own games, ensuring the game's longevity and continued evolution. It is a player-centered approach that makes for an enjoyable and engaging experience. See our docs for information on how to create your own game.",
-			'This platforn offers a variety of games for every gaming style, from classic RPGs to thrilling adventures. Our games have unique gameplay, frequent updates, and a user-friendly interface for easy game selection and socializing. Join our versatile hub for 2D MMO game enthusiasts.',
+			'This platform offers a variety of games for every gaming style, from classic RPGs to thrilling adventures. Our games have unique gameplay, frequent updates, and a user-friendly interface for easy game selection and socializing. Join our versatile hub for 2D MMO game enthusiasts.',
 		];
 
 		return (
